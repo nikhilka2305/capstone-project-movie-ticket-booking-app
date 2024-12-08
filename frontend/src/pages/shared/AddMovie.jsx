4e4f23d1af8ee03ba3579bc74ad55a2f010f7586
nikhilka <nikhilka23@gmail.com>
2024-12-08 15:10:51 +0530

    corrected some issues

--- frontend/src/pages/shared/AddMovie.jsx
+++ frontend/src/pages/shared/AddMovie.jsx
@@ -56,187 +56,176 @@ export default function AddMovie() {
 	return (
 		<section className="mx-auto my-8 w-full lg:w-2/3 flex flex-col gap-8 ">
 			<h2 className="text-center">Add New Movie</h2>
-			{loading && (
-				<div className="flex w-52 flex-col gap-4">
-					<div className="skeleton h-32 w-full"></div>
-					<div className="skeleton h-4 w-28"></div>
-					<div className="skeleton h-4 w-full"></div>
-					<div className="skeleton h-4 w-full"></div>
-				</div>
-			)}
-			{!loading && (
-				<>
-					<form
-						action=""
-						className="border rounded-md border-slate-900 py-8 bg-slate-200 dark:bg-slate-700 flex flex-col gap-4"
-						onSubmit={handleSubmit(handleAddMovie)}
-						noValidate
-					>
-						<Input
-							label="Enter Movie Name"
-							name="movieName"
-							id="movieName"
-							type="text"
-							register={register}
-							validationSchema={{
-								required: "Movie Title required",
-								minLength: {
-									value: 5,
-									message: "Please enter a minimum of 5 characters",
-								},
-							}}
-							errors={errors}
-						/>
-						<Input
-							label="Enter Release Date"
-							name="releaseDate"
-							id="releasedate"
-							type="date"
-							register={register}
-							validationSchema={{
-								required: "Date required",
-							}}
-							errors={errors}
-						/>
-						<Input
-							label="Enter Language"
-							name="language"
-							id="language"
-							type="text"
-							register={register}
-							validationSchema={{
-								required: "Language required",
-								minLength: {
-									value: 5,
-									message: "Please enter a minimum of 5 characters",
-								},
-							}}
-							errors={errors}
-						/>
-						<Input
-							label="Enter Genre"
-							name="genre"
-							id="genre"
-							type="text"
-							register={register}
-							validationSchema={{
-								required: "Genre required",
-								minLength: {
-									value: 5,
-									message: "Please enter a minimum of 5 characters",
-								},
-							}}
-							errors={errors}
-						/>
-						<Input
-							label="Enter Movie Duration"
-							name="movieduration"
-							id="movieduration"
-							type="number"
-							register={register}
-							validationSchema={{
-								required: "Duration required",
-								min: {
-									value: 30,
-									message: "Please enter a value of atleast 30",
-								},
-							}}
-							errors={errors}
-						/>
-						<Controller
-							name="rating"
-							control={control}
+
+			<form
+				action=""
+				className="border rounded-md border-slate-900 py-8 bg-slate-200 dark:bg-slate-700 flex flex-col gap-4"
+				onSubmit={handleSubmit(handleAddMovie)}
+				noValidate
+			>
+				<Input
+					label="Enter Movie Name"
+					name="movieName"
+					id="movieName"
+					type="text"
+					register={register}
+					validationSchema={{
+						required: "Movie Title required",
+						minLength: {
+							value: 5,
+							message: "Please enter a minimum of 5 characters",
+						},
+					}}
+					errors={errors}
+				/>
+				<Input
+					label="Enter Release Date"
+					name="releaseDate"
+					id="releasedate"
+					type="date"
+					register={register}
+					validationSchema={{
+						required: "Date required",
+					}}
+					errors={errors}
+				/>
+				<Input
+					label="Enter Language"
+					name="language"
+					id="language"
+					type="text"
+					register={register}
+					validationSchema={{
+						required: "Language required",
+						minLength: {
+							value: 5,
+							message: "Please enter a minimum of 5 characters",
+						},
+					}}
+					errors={errors}
+				/>
+				<Input
+					label="Enter Genre"
+					name="genre"
+					id="genre"
+					type="text"
+					register={register}
+					validationSchema={{
+						required: "Genre required",
+						minLength: {
+							value: 5,
+							message: "Please enter a minimum of 5 characters",
+						},
+					}}
+					errors={errors}
+				/>
+				<Input
+					label="Enter Movie Duration"
+					name="movieduration"
+					id="movieduration"
+					type="number"
+					register={register}
+					validationSchema={{
+						required: "Duration required",
+						min: {
+							value: 30,
+							message: "Please enter a value of atleast 30",
+						},
+					}}
+					errors={errors}
+				/>
+				<Controller
+					name="rating"
+					control={control}
+					defaultValue="U/A"
+					rules={{
+						required: "Please select a rating",
+					}}
+					render={({ field }) => (
+						<Select
+							label="Enter Rating"
+							field="rating"
+							options={["R", "U/A", "U", "A"]}
 							defaultValue="U/A"
-							rules={{
-								required: "Please select a rating",
-							}}
-							render={({ field }) => (
-								<Select
-									label="Enter Rating"
-									field="rating"
-									options={["R", "U/A", "U", "A"]}
-									defaultValue="U/A"
-									onChange={field.onChange}
-									errors={errors}
-								/>
-							)}
+							onChange={field.onChange}
+							errors={errors}
 						/>
+					)}
+				/>
 
-						<SelectActors
-							name="movieCast"
-							register={register}
-							setValue={setValue}
-							getValues={getValues}
-							maxNumber={5}
-							errors={errors}
-							validationSchema={{
-								minLength: {
-									value: 5,
-									message: "At least 5 characters required.",
-								},
-								maxLength: {
-									value: 20,
-									message: "Maximum 20 characters allowed.",
-								},
-							}}
-						/>
-						<Input
-							label="Enter Movie Director"
-							name="director"
-							id="director"
-							type="text"
-							register={register}
-							validationSchema={{
-								required: "Director required",
-								minLength: {
-									value: 5,
-									message: "Please enter a minimum of 5 characters",
-								},
-							}}
-							errors={errors}
-						/>
-						<Input
-							label="Add Movie Description"
-							name="movieDescription"
-							id="movieDescription"
-							type="textarea"
-							register={register}
-							validationSchema={{
-								required: "Description required",
-								minLength: {
-									value: 30,
-									message: "Please enter minimum of 30 characters",
-								},
-							}}
-							errors={errors}
-						/>
+				<SelectActors
+					name="movieCast"
+					register={register}
+					setValue={setValue}
+					getValues={getValues}
+					maxNumber={5}
+					errors={errors}
+					validationSchema={{
+						minLength: {
+							value: 5,
+							message: "At least 5 characters required.",
+						},
+						maxLength: {
+							value: 20,
+							message: "Maximum 20 characters allowed.",
+						},
+					}}
+				/>
+				<Input
+					label="Enter Movie Director"
+					name="director"
+					id="director"
+					type="text"
+					register={register}
+					validationSchema={{
+						required: "Director required",
+						minLength: {
+							value: 5,
+							message: "Please enter a minimum of 5 characters",
+						},
+					}}
+					errors={errors}
+				/>
+				<Input
+					label="Add Movie Description"
+					name="movieDescription"
+					id="movieDescription"
+					type="textarea"
+					register={register}
+					validationSchema={{
+						required: "Description required",
+						minLength: {
+							value: 30,
+							message: "Please enter minimum of 30 characters",
+						},
+					}}
+					errors={errors}
+				/>
 
-						<Input
-							label="Add poster Image"
-							name="posterImage"
-							id="posterImage"
-							type="file"
-							classes="file-input file-input-lg file-input-ghost w-full"
-							fileTypes={["image/jpeg", " image/jpg", " image/png"]}
-							register={register}
-							validationSchema={{
-								required: "Poster required",
-							}}
-							errors={errors}
-						/>
+				<Input
+					label="Add poster Image"
+					name="posterImage"
+					id="posterImage"
+					type="file"
+					classes="file-input file-input-lg file-input-ghost w-full"
+					fileTypes={["image/jpeg", " image/jpg", " image/png"]}
+					register={register}
+					validationSchema={{
+						required: "Poster required",
+					}}
+					errors={errors}
+				/>
 
-						<div className="button-group flex gap-4 justify-center">
-							<Button type="submit" label="Submit" />
-							<Button
-								label="Reset"
-								onClick={() => {
-									reset();
-								}}
-							/>
-						</div>
-					</form>
-				</>
-			)}
+				<div className="button-group flex gap-4 justify-center">
+					<Button type="submit" label="Submit" />
+					<Button
+						label="Reset"
+						onClick={() => {
+							reset();
+						}}
+					/>
+				</div>
+			</form>
 		</section>
 	);
 }
