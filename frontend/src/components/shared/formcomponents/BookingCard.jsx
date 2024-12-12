748c5bdd90422bde6576585b228188b4e8b495c2
nikhilka <nikhilka23@gmail.com>
2024-12-12 18:19:31 +0530

    Another styling update

--- frontend/src/components/shared/formcomponents/BookingCard.jsx
+++ frontend/src/components/shared/formcomponents/BookingCard.jsx
@@ -7,7 +7,7 @@ function BookingCard({
 }) {
 	return (
 		<div className="max-w-2xl min-w-xl mx-auto mt-8 mb-8 w-full text-wrap">
-			<div className="flex gap-3  border border-gray-300 rounded-xl overflow-hidden items-center justify-start p-2">
+			<div className="flex gap-3  border border-gray-300 rounded-xl overflow-hidden items-center justify-start p-2 bg-base-100 hover:bg-base-200 transition-all duration-300 dark:bg-slate-900 dark:hover:slate-800 hover:shadow-xl">
 				<div className="relative w-32 h-32 flex-shrink-0">
 					<img
 						className="absolute ml-4 rounded-md left-0 top-0 w-full h-full object-cover object-center transition duration-50"
